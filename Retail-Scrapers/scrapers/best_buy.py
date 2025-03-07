7efc5a751776609930b1b85ba762f70de41ee610
Marcos Campanella <mhmcamp@gmail.com>
2025-03-07 08:40:09 -0300

    Update best_buy.py

--- Retail-Scrapers/scrapers/best_buy.py
+++ Retail-Scrapers/scrapers/best_buy.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementClickInterceptedException, ElementNotInteractableException, StaleElementReferenceException
 from selenium_stealth import stealth
 import pandas as pd
 from scrapers.routines.Laundry.bb_file_cleaner import cleanup
